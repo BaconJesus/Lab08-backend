e7fb6b5376874b9811188b6cfe293c515e0f476c
BaconJesus <Kadnoknart_C@cmu.ac.th>
2021-09-03 00:11:34 +0700

    6.4

--- src/main/java/se331/lab/rest/dao/EventDaoDbImpl.java
+++ src/main/java/se331/lab/rest/dao/EventDaoDbImpl.java
@@ -23,8 +23,14 @@ public class EventDaoDbImpl implements EventDao {
         public Page<Event> getEvents(Integer pageSize, Integer page){
         return eventRepository.findAll(PageRequest.of(page -1, pageSize));
         }
-            @Override
-            public Event getEvent (Long id) {
-                return eventRepository.findById(id).orElse(null);
+        @Override
+        public Event getEvent (Long id) {
+        return eventRepository.findById(id).orElse(null);
             }
+
+        @Override
+        public Event save (Event event){
+        return eventRepository.save(event);
+    }
+
 }
